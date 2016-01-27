f22d2b7b943159ef2d5d1bebfc776df8fb9a4c97
Greg Palen <codingvirtual@gmail.com>
2016-01-27 09:14:20 -0600

    fix: replace array in MapViewController with NSFetchedResultsController fix: create FlickrAnnotation class that subclasses MKPointAnnotation in order to embed reference to the source Pin in the annotation

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 import CoreData
 import MapKit
 
-class MapViewController: UIViewController,  MKMapViewDelegate, UIGestureRecognizerDelegate {
+class MapViewController: UIViewController,  MKMapViewDelegate, UIGestureRecognizerDelegate, NSFetchedResultsControllerDelegate {
 	
 	
-	var fetchedResults = [Pin]()
+	lazy var fetchedResultsController: NSFetchedResultsController = {
+		
+		// Create the fetch request
+		let fetchRequest = NSFetchRequest(entityName: "Pin")
+		
+		fetchRequest.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+		
+		// Create the Fetched Results Controller
+		let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+		
+		// Return the fetched results controller. It will be the value of the lazy variable
+		return fetchedResultsController
+	} ()
+	
 	@IBOutlet weak var mapView: MKMapView!
 	
 	var sharedContext: NSManagedObjectContext {
@@ -24,17 +37,64 @@ class MapViewController: UIViewController,  MKMapViewDelegate, UIGestureRecogniz
 		super.viewDidLoad()
 		
 		// set up any additional UI here
-		fetchedResults = fetchAllPins()
-		print(fetchedResults.count)
-		// refresh the map with the stored pins from CoreData
-		if fetchedResults.count > 0 {
-			for aPin in self.fetchedResults {
-					let coords = CLLocationCoordinate2DMake((aPin.latitude as Double), (aPin.longitude as Double))
-					let annotation = MKPointAnnotation()
-					annotation.coordinate = coords
-					self.mapView.addAnnotation(annotation)
-				}
+		do {
+			try fetchedResultsController.performFetch()
+		} catch {
+			print("Unresolved error \(error)")
+			abort()
 		}
+		
+		// Change 2. Set this view controller as the fetched results controller's delegate
+		fetchedResultsController.delegate = self
+		
+		for aPin in fetchedResultsController.fetchedObjects as! [Pin] {
+			createPin(aPin)
+		}
+	}
+	
+	// MARK: - Fetched Results Controller Delegate
+	
+	//
+	// Change 3: Implement the delegate protocol methods
+	//
+	// These are the four methods that the Fetched Results Controller invokes on this view controller.
+	//
+	
+	func controllerWillChangeContent(controller: NSFetchedResultsController) {
+		// This invocation prepares the table to recieve a number of changes. It will store them up
+		// until it receives endUpdates(), and then perform them all at once.
+		print("controller will change content")
+	}
+	
+	func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+		// Our project does not use sections. So we can ignore these invocations.
+	}
+	
+
+	func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+		print(anObject)
+		switch type {
+		case .Insert:
+			createPin(anObject as! Pin)
+			self.mapView.reloadInputViews()
+		case .Delete:
+			deletePin(anObject as! Pin)
+		default:
+			return
+		}
+	}
+	
+	func createPin(pinData: Pin) {
+		// refresh the map with the stored pins from CoreData
+		
+
+		var annotation = FlickrAnnotation(withPin: pinData)
+
+		self.mapView.addAnnotation(annotation)
+	}
+	
+	func deletePin(thePin: Pin) {
+		
 	}
 	
 	@IBAction func longPress(sender: AnyObject) {
@@ -42,30 +102,9 @@ class MapViewController: UIViewController,  MKMapViewDelegate, UIGestureRecogniz
 		let recognizer: UILongPressGestureRecognizer = sender as! UILongPressGestureRecognizer
 		let point: CGPoint = recognizer.locationInView(mapView)
 		let locCoords: CLLocationCoordinate2D = mapView.convertPoint(point, toCoordinateFromView: mapView)
-		let annotation = MKPointAnnotation()
+		let annotation = FlickrAnnotation()
 		annotation.coordinate = locCoords
 		self.mapView.addAnnotation(annotation)
-	}
-	
-	
-	@IBAction func didClick(sender: AnyObject) {
-		print("adding pin")
-		addNewPin(27)
-	}
-	
-	@IBAction func fetchPins(sender: AnyObject) {
-		print(fetchAllPins().count)
-		
-	}
-	
-	func fetchAllPins() -> [Pin] {
-		let fetchRequest = NSFetchRequest(entityName: "Pin")
-		do {
-			return try self.sharedContext.executeFetchRequest(fetchRequest) as! [Pin]
-		} catch {
-			print("error")
-			return [Pin]()
-		}
 	}
 	
 	func addNewPin(id:NSNumber!) {
@@ -83,41 +122,12 @@ class MapViewController: UIViewController,  MKMapViewDelegate, UIGestureRecogniz
 	}
 	
 	// MARK: - MKMapViewDelegate
+
 	
-	// Here we create a view with a "right callout accessory view".
-	//	func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-	//
-	//		let reuseId = "pin"
-	//
-	//		var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
-	//
-	//		if pinView == nil {
-	//			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-	//			pinView!.canShowCallout = true
-	//			pinView!.pinTintColor = UIColor.redColor()
-	//			pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
-	//		}
-	//		else {
-	//			pinView!.annotation = annotation
-	//		}
-	//
-	//		return pinView
-	//	}
-	
-	
-	// This delegate method is implemented to respond to taps. It opens the system browser
-	// to the URL specified in the annotationViews subtitle property.
-	func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-		
-		if control == annotationView.rightCalloutAccessoryView {
-			// do something with tap
-			
-			// if mode = normal, segue to Photo Album
-			
-			// else if mode = edit, delete pin
-			
-		}
+	func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+		print("select")
+		let annotation = view.annotation as? FlickrAnnotation
+		print(annotation?.pin.id.stringValue)
 	}
-	
 }
 
